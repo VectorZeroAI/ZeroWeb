ebcd10c061aa8f9148330e3da4cc76ed76e1315d
VectorZeroAI <vectorzeroai@gmail.com>
2025-08-08 19:53:48 +0200

    Aktualisieren von config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 OPENROUTER_API_KEY = "your API key Here"
-MAX_TREAD_AMOUNT = "30" #change to the amount you want. the more, the more resourses this Programm would Take from your device.
+TREAD_AMOUNT_PER_CORE = "30" #You may even want a lot more, because each one of them is really, REALLY slow.
+CORE_AMOUNT = "4" #minimum 2 for the programm to funktion properly.
