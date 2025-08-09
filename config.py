89c2d72d5b9f2fe1ef67b9f69821a0541090e194
VectorZeroAI <vectorzeroai@gmail.com>
2025-08-09 10:15:32 +0200

    Aktualisieren von config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ OPENROUTER_API_KEY = "your API key Here"
 TREAD_AMOUNT_PER_CORE = "30" #You may even want a lot more, because each one of them is really, REALLY slow.
 CORE_AMOUNT = "4" #minimum 2 for the programm to funktion properly.
 DOMAINS = ["en.wikipedia.org","arxiv.org"]
+MAX_URLS_PER_DOMAIN = 100000000000
