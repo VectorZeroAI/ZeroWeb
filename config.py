47467a964cbfa7c1e9b0bbbcb23a6c3b188a1317
VectorZeroAI <vectorzeroai@gmail.com>
2025-08-09 10:40:07 +0200

    Aktualisieren von config.py

--- config.py
+++ config.py
@@ -3,3 +3,16 @@ TREAD_AMOUNT_PER_CORE = "30" #You may even want a lot more, because each one of 
 CORE_AMOUNT = "4" #minimum 2 for the programm to funktion properly.
 DOMAINS = ["en.wikipedia.org","arxiv.org"]
 MAX_URLS_PER_DOMAIN = 100000000000
+
+DATABASE_CONFIG = {
+    'dbname': 'your_database_name',
+    'user': 'your_database_user',
+    'password': 'your_password',
+    'host': 'localhost',  # or your DB server address
+    'port': 5432           # default PostgreSQL port
+}
+
+#IMPORTANT!!! YOU NEED TO SET UP A POSTGRESQL DB FIRST! Google how that works if you dont know, it is easy.
+
+SCRAPING_THREADS_PER_PROCESS = THREAD_AMOUNT_PER_CORE
+MAX_SCRAPING_PROCESSES = CORE_AMOUNT
